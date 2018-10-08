dfcbb8e056bb10baef3f8abf41e7d2d28ea95246
birukyemane <bir_yem@yahoo.com>
2018-10-08 21:10:29 +0300

    resizeing image done,

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,8 @@
 let slide = {
   allPhotos: photos,
   currentPhotoIndex:0,
+  photoBaseWidth: 250, // defailt width
+  widthIncrement: 0,
   showItem : function () {
     let currentElement = this.allPhotos[this.currentPhotoIndex];
     let picture = '', skills = '', moreinfo = '', name = '', favoriteQuote = '';
@@ -20,15 +22,16 @@ let slide = {
           <div id="skills" class="dontShow">${this.skills(currentElement.skills)}</div>
         </div>
       </div>`;
-      title = `<div id="title">${currentElement.title}</div>`;
-      picture =  `<div id="imgWrapper"><img id ="itemImage" src="./images/${currentElement.src}" alt="${currentElement.alt}">
+      title = `<div id="title">${currentElement.title}</div>`;   
+      picture =  `<div id="imgWrapper"><img id ="itemImage" style = "width:${(this.photoBaseWidth + this.widthIncrement)+'px'}" src="./images/${currentElement.src}" alt="${currentElement.alt}">
       <i id="forward" class="fa fa-forward" onclick="slide.nextPic()"></i> <i id="backward" class="fa fa-backward" onclick="slide.previousPic()"></i>
-      </div>`;
-      name =  `<div id="name">${currentElement.firstName} ${currentElement.lastName}</div>`; 
+      <div id="name">${currentElement.firstName} ${currentElement.lastName}</div>
+      </div>`;     
       favoriteQuote = `<div id="quote">"${currentElement.favoriteQuote}"</div>`;
     }   
-    document.querySelector('#item').innerHTML = `<div id="pictureSection">${picture} ${name} 
-    <button class="buttons" onclick="slide.toggleViews()">Details</button></div>
+    document.querySelector('#item').innerHTML = `<div id="pictureSection">${picture} <div id="indicator">${this.currentPhotoIndex + 1}/${this.allPhotos.length}</div>
+    <button class="buttons" onclick="slide.toggleViews()">Details</button>
+    <input type="range" min="1" max="50" value="${this.widthIncrement}" class="slider" id="myRange" onchange="slide.resizeImg()"></div>
     <div id="info" class="dontShow">${title}${favoriteQuote}${moreinfo}</div>  
     `;              
   },
@@ -72,14 +75,24 @@ let slide = {
       document.querySelector('.buttons').textContent = 'Details';
       document.querySelector('#forward').style.display = 'inline-block';
       document.querySelector('#backward').style.display = 'inline-block';
+      document.querySelector('#indicator').style.display = 'inline-block';
+      document.querySelector('#myRange').style.display = 'inline-block';
     } else {
       document.querySelector('.buttons').textContent = 'Back to gallary';
       document.querySelector('#forward').style.display = 'none';
       document.querySelector('#backward').style.display = 'none';
+      document.querySelector('#indicator').style.display = 'none';
+      document.querySelector('#myRange').style.display = 'none';
       document.querySelector('#accordionPlusSign').style.display = 'inline-block';
       document.querySelector('#accordionMinusSign').style.display = 'none';
       document.querySelector('#skills').classList.remove('active');
     }
+  },
+  resizeImg: function() {
+    this.widthIncrement = Number(document.querySelector('#myRange').value);
+    let newWidth = this.photoBaseWidth + this.widthIncrement; // 250 is the base width or default
+    console.log(newWidth,this.widthIncrement,document.querySelector('#itemImage').offsetWidth);
+    document.querySelector('#itemImage').style.width = newWidth + 'px';
   }
 }
 
